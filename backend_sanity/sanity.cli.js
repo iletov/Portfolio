4ecd313e766657eb1ad46f092b70b37ca9a3e22b
Ivan Letov <letov88@gmail.com>
2023-04-19 00:05:40 +0300

    Portfolio

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'a259lrr7',
+    dataset: 'production'
+  }
+})
---
+++ frontend_react/src/container/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
