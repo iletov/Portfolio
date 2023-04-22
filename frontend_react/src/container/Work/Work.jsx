299cb7f852824d7b5da8d4075e0cab6745b9dab0
Ivan Letov <letov88@gmail.com>
2023-04-22 23:40:30 +0300

    Skills & Experience

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
-import { AppWrapp } from '../../wrapper';
+import { AppWrapp, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 
 import './Work.scss';
@@ -113,4 +113,7 @@ const Work = () => {
   )
 }
 
-export default AppWrapp(Work, 'work');
+export default AppWrapp(
+  MotionWrap(Work, 'app__works'),
+  'work', "app__primarybg"
+  );
